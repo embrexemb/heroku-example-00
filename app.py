cbeb1e0c337f5bf95ed26e342875a6b6fe82f56b
Eve Barkley <evebarkley@icloud.com>
2021-06-25 08:02:40 -0400

    changed back

--- app.py
+++ app.py
@@ -1,53 +1,21 @@
-from flask import Flask, jsonify, render_template
-from dotenv import load_dotenv
-from os import environ
+from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from flask_pymongo import PyMongo
+from os import environ
 
-load_dotenv()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URI')
-app.config['MONGO_URI'] = environ.get('MONGODB_URI')
+#app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URI')
+#app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URI','sqlite:///notepad.sqlite')
 
-# database setup
 db = SQLAlchemy(app)
-mongo = PyMongo(app)
 
-class Note(db.Model):
+class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String)
+    description = db.Column(db.String)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
-
-@app.route('/api/notes/postgres')
-def note_postgres():
-    notes = db.session.query(Note)
-    data = []
-
-    for note in notes:
-        data.append({
-            "id": note.id,
-            "content": note.content
-        })
-
-    return jsonify(data)
-
-
-@app.route('/api/notes/mongo')
-def note_mongo():
-    notes = mongo.db.notes.find()
-    data = []
-
-    for note in notes:
-        data.append({
-            '_id': str(note['_id']),
-            'content': note['content']
-        })
-
-    return jsonify(data)
+    return 'Hi Team - our first API on Heroku worked!!!!  - More coming today'
 
 
 if __name__ == '__main__':
---
+++ teacher_example/app.py
@@ -0,0 +1,54 @@
+from flask import Flask, jsonify, render_template
+from dotenv import load_dotenv
+from os import environ
+from flask_sqlalchemy import SQLAlchemy
+from flask_pymongo import PyMongo
+
+load_dotenv()
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URI')
+app.config['MONGO_URI'] = environ.get('MONGODB_URI')
+
+# database setup
+db = SQLAlchemy(app)
+mongo = PyMongo(app)
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/api/notes/postgres')
+def note_postgres():
+    notes = db.session.query(Note)
+    data = []
+
+    for note in notes:
+        data.append({
+            "id": note.id,
+            "content": note.content
+        })
+
+    return jsonify(data)
+
+
+@app.route('/api/notes/mongo')
+def note_mongo():
+    notes = mongo.db.notes.find()
+    data = []
+
+    for note in notes:
+        data.append({
+            '_id': str(note['_id']),
+            'content': note['content']
+        })
+
+    return jsonify(data)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
