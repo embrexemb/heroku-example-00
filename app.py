d9367cfdf08b450a675937953278fabc608d9c80
Eve Barkley <evebarkley@icloud.com>
2021-06-24 20:59:07 -0400

    db adds

--- app.py
+++ app.py
@@ -1,9 +1,33 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from os import environ
+
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL','sqlite:///notepad.sqlite')
+
+db = SQLAlchemy(app)
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String)
 
 @app.route('/')
 def index():
     return 'Hi Team - our first API on Heroku worked!!!!'
+
+@app.route('/api/tasks-postgres')
+def tasksPostGres():
+    tasks = db.session.query(Task)
+    data = []
+
+    for task in tasks:
+        item = {
+            'id':task_id,
+            'description':task.description
+        }
+        data.append(item)
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
